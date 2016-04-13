3369476aa1a7aef292233c66f7d7df94775aa43d
jlg317 <jlg317@lehigh.edu>
2016-04-13 19:29:50 -0400

    Added files via upload

--- ProtoGUI/src/modeltest/ProductDescription.java
+++ ProtoGUI/src/modeltest/ProductDescription.java
@@ -13,6 +13,7 @@ import java.sql.*;
 class ProductDescription {
     private String desc;
     private double salePrice;
+    private double rentPrice;
     private String id;
     private boolean validItem;
     String sql;
@@ -20,12 +21,23 @@ class ProductDescription {
     
     ProductDescription(String id, Connection conn) throws ClassNotFoundException, SQLException {
         Statement s = conn.createStatement();
-        int select = Integer.parseInt(id);
+        int select = Integer.parseInt(id); 
         sql = "SELECT name, price FROM scrum.inventory where itemid = " + select;
         rs = s.executeQuery(sql);
         while (rs.next()) {
            this.desc = rs.getString("name");
            this.salePrice = rs.getDouble("price");
+        }
+        if (desc == null){
+            this.validItem = false;
+        }else{
+            this.validItem = true; //true or false if it exists in db
+        }
+        sql = "SELECT name, rentprice FROM scrum.inventory where itemid = " + select;
+        rs = s.executeQuery(sql);
+        while (rs.next()) {
+           this.desc = rs.getString("name");
+           this.rentPrice = rs.getDouble("price");
         }
         if (desc == null){
             this.validItem = false;
@@ -41,6 +53,10 @@ class ProductDescription {
     
     double getSalePrice() {
         return this.salePrice;
+    }
+    
+    double getRentPrice(){
+        return this.rentPrice;
     }
     
     boolean isValid() {
